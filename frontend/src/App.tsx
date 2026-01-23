1c43d7f910c7dc00a98a4f70fc53f1aec4a0c527
Geoffrey Lee <geoffrey0953@gmail.com>
2026-01-23 13:19:03 -0800

    Added sample main.py and installed react and tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <button className="bg-blue-600 hover:bg-blue-700 text-white text-4xl font-bold py-8 px-16 rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+          Hello World!
+      </button>
+    </div>
+  )
+}
+
+export default App
